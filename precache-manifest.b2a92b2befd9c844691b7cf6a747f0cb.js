78cf609acc47a361cf70bd45848f1be5c84eedd0
sungmin.jung <jspiner@naver.com>
2019-11-23 19:28:50 +0900

    Updates

---
+++ precache-manifest.b2a92b2befd9c844691b7cf6a747f0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616fb102efb1aed794ab4717d69f9ba5",
+    "url": "/swim-book-frontend/index.html"
+  },
+  {
+    "revision": "102cbc70c3d02d0bd843",
+    "url": "/swim-book-frontend/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "751d90eeb150cbb77ab1",
+    "url": "/swim-book-frontend/static/css/main.86255cd7.chunk.css"
+  },
+  {
+    "revision": "102cbc70c3d02d0bd843",
+    "url": "/swim-book-frontend/static/js/2.c7945f2a.chunk.js"
+  },
+  {
+    "revision": "751d90eeb150cbb77ab1",
+    "url": "/swim-book-frontend/static/js/main.3ffdf866.chunk.js"
+  },
+  {
+    "revision": "9823a668bb80eca0602c",
+    "url": "/swim-book-frontend/static/js/runtime-main.68576770.js"
+  }
+]);
